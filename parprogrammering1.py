f620b39471edf377250452d59fd28b86767b7e1c
viktor persson <viktor.persson.1@skola.botkyrka.se>
2020-01-20 09:43:13 +0100

    biggest number

--- parprogrammering1.py
+++ parprogrammering1.py
@@ -2,3 +2,8 @@ number = [12, 34, 16, 83]
 smallest_number = min(number)
 
 print("Den minsta siffran är", smallest_number)
+
+big = [12, 34, 56]
+biggest_number = max(big)
+
+print("Den största siffran är ", biggest_number)
